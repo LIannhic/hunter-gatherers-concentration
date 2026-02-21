4e369d19d4f5413922a09f7a45283e90507161b7
LIannhic <154102382+LIannhic@users.noreply.github.com>
2026-02-21 18:04:06 +0100

    feat: open a window for the game

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// Game implémente l'interface ebiten.Game
+type Game struct{}
+
+// Update contient la logique (entrées clavier/manette, calculs)
+func (g *Game) Update() error {
+	return nil
+}
+
+// Draw contient le rendu (affichage des tuiles, texte)
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+// Layout définit la taille de la fenêtre
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480 // longueur, hauteur
+}
 
 func main() {
-	fmt.Println("Hunter-Gatherers Engine starting...")
+	game := &Game{}
+	ebiten.SetWindowTitle("Hunter-Gatherers-concentration")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
